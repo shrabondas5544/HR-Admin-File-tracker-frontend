b9e4ae3877576ac566d4f8d5448f08bb0ab88a04
shrabondas5544 <shrabondas5544@gmail.com>
2026-09-23 14:55:53 +0600

    chore: allow cloudflare tunnel hosts in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    allowedHosts: ['.trycloudflare.com'],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
